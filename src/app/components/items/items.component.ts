c838f518ca13d6c18e31393b58fa11616f6adb22
kote <kotekirkitadze@gmail.com>
2021-06-23 22:25:37 +0400

    full version

--- src/app/components/items/items.component.ts
+++ src/app/components/items/items.component.ts
@@ -11,6 +11,8 @@ import { Item } from "../../models/item"
 export class ItemsComponent implements OnInit {
 
   items: Item[];
+  editState: boolean = false;
+  itemToEdit: Item;
 
   //constructor mostly is used for injecting
   constructor(private itemService: ItemService) { }
@@ -26,7 +28,25 @@ export class ItemsComponent implements OnInit {
   }
 
   deleteItem(event, item){
+    //state imitom unda gamovidzaxot, washlis mere
+    //state rom gavaqrot da zed gamodzaxebuli funqciebi erorshi
+    //ar gamovidesa an itemToEditze ar sherches washlili mnishvneloba
+    this.clearState(); 
     this.itemService.deleteItemCollection(item);
   }
 
+  editItem(event, item: Item){
+    this.editState = true;
+    this.itemToEdit = item;
+  }
+
+  updateItem(item: Item){
+    this.itemService.updateItem(item);
+    this.clearState();
+  }
+
+  clearState() {
+    this.editState = false;
+    this.itemToEdit = null;
+  }
 }
